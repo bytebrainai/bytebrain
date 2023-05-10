568d9d6406d343c800f1dae907068d8198f6734d
Milad Khajavi <khajavi@gmail.com>
2023-05-10 15:16:26 +0330

    add zio chat client.

---
+++ zio-chat-client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ChatPrompt from './stories/ChatPrompt/ChatPrompt';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ChatPrompt
+        title="ZIO Chat"
+        defaultQuestion="What are the befenits of using ZIO?"
+        websocketEndpoint="ws://localhost:8081/chat"
+     />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
