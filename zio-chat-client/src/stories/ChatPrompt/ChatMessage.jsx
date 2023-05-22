7c2947075d14423851d97466bdc1d4ac75a3eef2
Milad Khajavi <khajavi@gmail.com>
2023-05-22 16:02:33 +0330

    add space between paragraphs.
    Former-commit-id: ba132369edc4205e9303e66a9ff29d5fbc6e7944

--- zio-chat-client/src/stories/ChatPrompt/ChatMessage.jsx
+++ zio-chat-client/src/stories/ChatPrompt/ChatMessage.jsx
@@ -26,7 +26,7 @@ function ChatMessage(props) {
         <div className="flex items-end">
           <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
             <div>
-              <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white">
+              <span className="px-4 py-2 space-y-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white">
                 <ReactMarkdown>{props.text}</ReactMarkdown>
               </span>
             </div>
