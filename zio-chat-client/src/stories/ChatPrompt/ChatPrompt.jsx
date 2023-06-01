d827616a9446f4a5cdd166e1d30cbdc397e85474
Milad Khajavi <khajavi@gmail.com>
2023-06-01 16:50:37 +0330

    integrate with letsencrypt-docker-compose.

--- zio-chat-client/src/stories/ChatPrompt/ChatPrompt.jsx
+++ zio-chat-client/src/stories/ChatPrompt/ChatPrompt.jsx
@@ -33,8 +33,9 @@ export const ChatPrompt = (props) => {
 
   const handleClick = () => {
     const host = websocketHost || window.location.hostname;
-    const port = websocketPort === 80 ? "" : ":" + websocketPort;
-    const ws = new WebSocket("ws://" + host+ port + websocketEndpoint)
+    const port = websocketPort === "80" ? "" : ":" + websocketPort;
+    const protocol = websocketPort === "80" ? "wss://" : "ws://";
+    const ws = new WebSocket(protocol + host+ port + websocketEndpoint);
     ws.onopen = (event) => {
       setMessages(messages => messages.concat([
         {
