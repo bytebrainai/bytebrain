17735a8ed5f1e69588901e59ed2f0670248ed913
Milad Khajavi <khajavi@gmail.com>
2023-06-08 18:49:25 +0330

    a simple working example of discord bot.

---
+++ zio-chat-server/zio_chat/discord.py
@@ -0,0 +1,25 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+
+def main():
+    client.run(token=os.environ['DISCORD_BOT_TOKEN'])
