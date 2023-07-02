020f0d38faa866f40babe3f78b9c42a83603188a
Milad Khajavi <khajavi@gmail.com>
2023-07-02 13:25:27 +0330

    crawl zio.dev

---
+++ zio-chat-server/zio_chat/dev/crawl.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+
+def crawl_website(url):
+    visited_urls = set()
+
+    def crawl(url):
+        response = requests.get(url)
+        html_content = response.content
+
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        # Extract all URLs from the page
+        all_urls = set()
+        for tag in soup.find_all(["a"]):
+            href = tag.get("href")
+            if href and href.startswith("/"):
+                all_urls.add(f"https://zio.dev{href}")
+
+        # Process the URLs as needed
+        for new_url in all_urls:
+            if new_url not in visited_urls:
+                visited_urls.add(new_url)
+                print(new_url)
+                crawl(new_url)
+
+    crawl(url)
+
+
+def main():
+    crawl_website("https://zio.dev")
