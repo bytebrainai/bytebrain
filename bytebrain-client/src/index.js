cdf8f45aa0540accb5d668e374731e5e5ae04ba0
Milad Khajavi <khajavi@gmail.com>
2023-08-13 16:43:19 +0330

    fix fullscreen issue.
    Former-commit-id: 13445c94dc91886a3d9a9ce28744b4c6856dc709

--- bytebrain-client/src/index.js
+++ bytebrain-client/src/index.js
@@ -24,7 +24,7 @@ root.render(
       websocketPort="80"
       websocketEndpoint="/chat"
       welcomeMessages={welcome_messages}
-      fullscreen={true}
+      fullScreen={true}
     />
   </React.StrictMode>
 );
