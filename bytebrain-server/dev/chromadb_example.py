aa368ef34bdcd94ca33db557194fac5a4b1f28cc
Milad Khajavi <khajavi@gmail.com>
2023-09-05 15:16:29 +0330

    index docs with their ids.

---
+++ bytebrain-server/dev/chromadb_example.py
@@ -0,0 +1,27 @@
+from langchain.schema import Document
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+# def run():
+# doc = Document(page_content="something very very new")
+# embeddings: OpenAIEmbeddings = OpenAIEmbeddings()
+# Chroma.from_documents(documents=[doc], embedding=embeddings, ids=["s1"], persist_directory="./mydb")
+# chroma = Chroma(embedding_function=embeddings, persist_directory='./db-discord')
+# try:
+#     chroma.update_document(document=doc, document_id="x")
+# except ValueError:
+#     chroma.add_documents(documents=[doc])
+# print("done!")
+
+import pandas as pd
+
+# Provide the path to the Parquet file
+parquet_file_path = './db-discord/chroma-embeddings.parquet'
+
+# Read the Parquet file into a pandas DataFrame
+data_frame = pd.read_parquet(parquet_file_path)
+
+    # Now you can work with the data in the DataFrame
+
+
--- bytebrain-server/index/index.py
+++ bytebrain-server/index/index.py
@@ -1,7 +1,7 @@
 import json
 import os
 import tempfile
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 from urllib.error import HTTPError
 
 import googleapiclient.discovery
@@ -16,10 +16,17 @@ from langchain.schema import Document
 from langchain.text_splitter import MarkdownTextSplitter
 from langchain.vectorstores import Chroma
 
+from config import load_config
+from structlog import getLogger
 
-def update_vectorestore(texts: list[Document]):
+config = load_config()
+
+log = getLogger()
+
+
+def update_db(texts: list[Document], ids: Optional[List[str]] = None):
     embeddings: OpenAIEmbeddings = OpenAIEmbeddings()
-    Chroma.from_documents(texts, embeddings, persist_directory=os.environ["ZIOCHAT_CHROMA_DB_DIR"])
+    Chroma.from_documents(texts, embeddings, ids=ids, persist_directory=config.db_dir)
 
 
 def extract_metadata(md_file_path: str) -> Dict[str, str]:
@@ -36,51 +43,152 @@ def extract_metadata(md_file_path: str) -> Dict[str, str]:
         return {"id": file_name, "title": file_name}
 
 
-def index_markdown_docs(directory: str):
+def load_zio_website_docs(directory: str) -> List[Document]:
     documents: list[Document] = []
 
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith('.md'):
                 md_path = os.path.join(root, filename)
-                print(md_path)
-
+                log.info(md_path)
                 docs: list[Document] = UnstructuredMarkdownLoader(md_path).load()
-
                 metadata: dict[str, str] = extract_metadata(md_path)
-                absolute_path = root.split("/zio/docs/")[1] + "/" + metadata["id"]
-
-                for doc in docs:
-                    doc.metadata.setdefault("source_doc", "zio.dev")
-                    doc.metadata.setdefault("url", f"https://zio.dev/{absolute_path}")
-                    doc.metadata.setdefault("title", metadata["title"])
-
+                for index, doc in enumerate(docs):
+                    doc.metadata["doc_path"] = doc.metadata.pop("source").split("/zio/docs/")[1]
+                    doc.metadata.setdefault("doc_source", "zio.dev")
+                    doc.metadata.setdefault("doc_id", root.split("/zio/docs/")[1] + '/' + metadata["id"])
+                    doc.metadata.setdefault("doc_title", metadata["title"])
                 documents.extend(docs)
 
-    texts: list[Document] = MarkdownTextSplitter().split_documents(documents)
-    return texts
+    return MarkdownTextSplitter().split_documents(documents)
+
+
+def generate_ids_for_zionomicon_docs(docs: List[Document]) -> List[str]:
+    ids: List[str] = []
+
+    current_file_name = None
+    index = 0
+
+    for i, current in enumerate(docs):
+        doc_path = current.metadata['doc_path']
+
+        if doc_path != current_file_name:
+            current_file_name = doc_path
+            index = 0
+
+        ids.append(f"zionomicon/{doc_path}/{index}")
+        index += 1
+
+    assert (len(ids) == len(docs))
+    return ids
+
+
+def generate_ids_for_website_docs(docs: List[Document]) -> List[str]:
+    ids: List[str] = []
+
+    current_page_id = None
+    index = 0
+
+    for i, current in enumerate(docs):
+        doc_id = current.metadata['doc_id']
+
+        if doc_id != current_page_id:
+            current_page_id = doc_id
+            index = 0
+
+        ids.append(f"zio.dev/{doc_id}/{index}")
+        index += 1
+
+    assert (len(ids) == len(docs))
+    return ids
 
 
 def index_zio_project_docs():
-    docs = index_markdown_docs(os.environ["ZIOCHAT_DOCS_DIR"])
-    update_vectorestore(docs)
+    docs = load_zio_website_docs(os.environ["ZIOCHAT_DOCS_DIR"])[1:10]
+    ids = generate_ids_for_website_docs(docs)
+    update_db(docs, ids)
+
+
+def load_zionomicon_docs(directory: str) -> List[Document]:
+    documents: list[Document] = []
+
+    chapters = {
+        "05-integrating-with-zio.md": "Essentials: Integrating With ZIO",
+        "22-advanced-stm.md": "Software Transactional Memory: Advanced STM",
+        "34-assertions.md": "Testing: Assertions",
+        "21-stm-data-structures.md": "Software Transaction Memory: STM Data Structures",
+        "07-concurrency-operators.md": "Parallelism And Concurrency: Concurrency Operators",
+        "24-debugging.md": "Advanced Error Management: Debugging",
+        "33-basic-testing.md": "Testing: Basic Testing",
+        "40-reporting.md": "Testing: Reporting",
+        "02-first-steps-with-zio.md": "Essentials: First Steps With ZIO",
+        "48-applications-spark.md": "Applications: Spark",
+        "13-hub-broadcasting.md": "Concurrent Structures: Hub - Broadcasting",
+        "49-appendix-a-the-scala-type-system.md": "Appendix: The Scala Type System",
+        "30-combining-streams.md": "Streaming: Combining Streams",
+        "03-testing-zio-programs.md": "Essentials: Testing ZIO Programs",
+        "29-transforming-streams.md": "Streaming: Transforming Streams",
+        "26-first-steps-with-zstream.md": "Streaming: First Steps With ZStream",
+        "39-test-annotations.md": "Testing: Test Annotations",
+        "38-property-based-testing.md": "Testing: Property Based Testing",
+        "32-sinks.md": "Streaming: Sinks",
+        "10-references-functional-descriptions-of-mutable-state.md": "Concurrent Structures: Ref - Shared State",
+        "11-promise-work-synchronization.md": "Concurrent Structures: Promise - Work Synchronization",
+        "41-applications-parallel-web-crawler.md": "Applications: Parallel Web Crawler",
+        "08-fiber-supervision-in-depth.md": "Parallelism And Concurrency: Fiber Supervision In Depth",
+        "06-the-fiber-model.md": "Parallelism And Concurrency: The Fiber Model",
+        "25-best-practices.md": "Advanced Error Management: Best Practices",
+        "47-applications-graphql-api.md": "Applications: GraphQL API",
+        "14-semaphore-work-limiting.md": "Concurrent Structures: Semaphore - Work Limiting",
+        "31-pipelines.md": "Streaming: Pipelines",
+        "45-applications-grpc-microservices.md": "Applications: gRPC Microservices",
+        "44-applications-kafka-stream-processor.md": "Applications: Kafka Stream Processor",
+        "28-channels.md": "Channels: Unifying Streams, Sinks, and Pipelines",
+        "20-stm-composing-atomicity.md": "Software Transactional Memory: Composing Atomicity",
+        "36-test-aspects.md": "Testing: Test Aspects",
+        "16-scope-composable-resources.md": "Resource Handling: Scope - Composable Resources",
+        "42-applications-file-processing.md": "Applications: File Processing",
+        "46-applications-rest-api.md": "Applications: REST API",
+        "43-applications-command-line-interface.md": "Applications: Command Line Interface",
+        "27-next-steps-with-zstream.md": "Streaming: Next Steps With ZStream",
+        "01-foreword.md": "Foreword by John A. De Goes",
+        "50-appendix-b-mastering-variance.md": "Appendix: Mastering Variance",
+        "15-acquire-release-safe-resource-handling-for-asynchronous-code.md": "Resource Handling: Acquire Release - Safe Resource Handling",
+        "23-retries.md": "Advanced Error Management: Retries",
+        "12-queue-work-distribution.md": "Concurrent Structures: Queue - Work Distribution",
+        "35-the-test-environment.md": "Testing: The Test Environment",
+        "04-the-zio-error-model.md": "Essentials: The ZIO Error Model",
+        "09-interruption-in-depth.md": "Parallelism And Concurrency: Interruption In Depth",
+        "19-advanced-dependency-injection.md": "Dependency Injection: Advanced Dependency Injection",
+        "18-dependency-injection-essentials.md": "Dependency Injection: Essentials",
+        "37-using-resources-in-tests.md": "Testing: Using Resources In Tests",
+        "17-advanced-scopes.md": "Resource Handling: Advanced Scopes",
+    }
+
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            if file_name.endswith('.md'):
+                md_path = os.path.join(root, file_name)
+                log.info(md_path)
+                docs: list[Document] = UnstructuredMarkdownLoader(md_path).load()
+                for index, doc in enumerate(docs):
+                    doc.metadata.setdefault("doc_source", "zionomicon")
+                    doc.metadata.setdefault("doc_path", doc.metadata.pop('source').split("/zionomicon/docs/")[1])
+                    doc.metadata.setdefault("doc_chapter", chapters[file_name])
+                documents.extend(docs)
+
+    return MarkdownTextSplitter().split_documents(documents)
 
 
 def index_zionomicon_book():
-    docs = index_markdown_docs(os.environ["ZIOCHAT_ZIONOMICON_DOCS_DIR"])
-    update_vectorestore(docs)
+    docs = load_zionomicon_docs(os.environ["ZIOCHAT_ZIONOMICON_DOCS_DIR"])
+    ids = generate_ids_for_zionomicon_docs(docs)
+    update_db(docs, ids)
 
 
 def index_zio_project_source_code():
-    loader = GitLoader(
-        repo_path=os.environ["ZIOCHAT_ZIO_REPO_DIR"],
-        branch="series/2.x",
-        file_filter=lambda file_path: file_path.endswith(".scala")
-    )
-    docs = loader.load()
-    for doc in docs:
-        print(doc.json())
-    update_vectorestore(docs)
+    ids, docs = load_source_code(repo_path=os.environ["ZIOCHAT_ZIO_REPO_DIR"], branch="series/2.x")
+    update_db(docs, ids)
 
 
 def clone_repo(repo_url, depth=1) -> str:
@@ -102,17 +210,25 @@ def index_zio_ecosystem_source_code():
     for prj in zio_ecosystem_clone_urls():
         print(prj)
         prj_dir = clone_repo(prj['clone_url'])
-        index_source_code(prj_dir, prj['default_branch'])
+        ids, docs = load_source_code(prj_dir, prj['default_branch'])
+        update_db(docs, ids)
 
 
-def index_source_code(repo_path: str, branch: Optional[str]):
+def load_source_code(repo_path: str, branch: Optional[str]) -> (List[str], List[Document]):
     loader = GitLoader(
         repo_path=repo_path,
         branch=branch,
         file_filter=lambda file_path: file_path.endswith(".scala")
     )
     docs = loader.load()
-    update_vectorestore(docs)
+
+    ids: List[str] = []
+    for doc in docs:
+        id = doc.metadata['file_path']
+        ids.append(id)
+
+    assert (len(ids) == len(docs))
+    return ids, docs
 
 
 def list_of_channel_videos():
@@ -181,7 +297,7 @@ def index_youtube_video():
             docs: list[Document] = loader.load()
             # TODO: All the transcript is loaded into one document
             #       We need to split that into multiple smaller documents
-            update_vectorestore(docs)
+            update_db(docs)
         except Exception:
             print("Failed to index video id: " + video_id)
 
