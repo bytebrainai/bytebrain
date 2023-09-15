ee4a7e1a5ea2bc9596aa2e60c2f53fcf3d72b146
Milad Khajavi <khajavi@gmail.com>
2023-09-15 20:20:29 +0330

    update sqlite3 version.

--- bytebrain-server/core/main.py
+++ bytebrain-server/core/main.py
@@ -5,6 +5,7 @@ from core.discord import main
 
 
 def start():
+    load_sqlite3()
     p1 = multiprocessing.Process(target=core.webservice.main)
     p2 = multiprocessing.Process(target=core.discord.main)
 
@@ -13,3 +14,14 @@ def start():
 
     p1.join()
     p2.join()
+
+
+def load_sqlite3():
+    import subprocess
+    import sys
+
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", "pysqlite3-binary"]
+    )
+    __import__("pysqlite3")
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
