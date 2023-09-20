8fcb608fbd8cc1496eba3b76f3379c58ca03d3d6
Milad Khajavi <khajavi@gmail.com>
2023-09-20 13:35:41 +0330

    fix issue running discord bot.

--- bytebrain-server/core/main.py
+++ bytebrain-server/core/main.py
@@ -1,12 +1,12 @@
 import multiprocessing
 import core
 from core.webservice import main
-from core.discord import main
+from core.discord_bot import main
 
 
 def start():
     p1 = multiprocessing.Process(target=core.webservice.main)
-    p2 = multiprocessing.Process(target=core.discord.main)
+    p2 = multiprocessing.Process(target=core.discord_bot.main)
 
     p1.start()
     p2.start()
