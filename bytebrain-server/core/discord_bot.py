02524e5309f12763e5d4b35d3bccdee76899f89f
Milad Khajavi <khajavi@gmail.com>
2023-09-20 14:39:31 +0330

    removed client.
    Former-commit-id: 128889dc10dcf177691fdc1c7c7889f1eda3828e

--- bytebrain-server/core/discord_bot.py
+++ bytebrain-server/core/discord_bot.py
@@ -9,7 +9,6 @@ from typing import List, Optional
 
 import chat_exporter
 import discord
-from discord import Client
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 from discord.guild import Guild
@@ -32,7 +31,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-client: Client = discord.Client(intents=intents)
 bot: Bot = commands.Bot(command_prefix="!", intents=intents)
 log = getLogger()
 
