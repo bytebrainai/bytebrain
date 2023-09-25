7db50d58112ecd1ef6d555b51c475ead7d450e86
Milad Khajavi <khajavi@gmail.com>
2023-09-25 13:32:44 +0330

    clean-up recent works.
    Former-commit-id: 385b38df286ca15f1a50993b309050e7cd7a5b90

--- bytebrain-server/dev/sample_text_splitter.py
+++ bytebrain-server/dev/sample_text_splitter.py
@@ -1,9 +1,9 @@
 import hashlib
-from typing import Optional, List, Dict, Any, Tuple
+from typing import Optional, List, Dict
 
 from langchain.document_loaders import GitLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 
 
 def calculate_md5_checksum(text):
@@ -32,15 +32,20 @@ def get_original_ids(db_dir: str, docs_type: str, source_identifier: str) -> Lis
         print("sqlite error: ", e)
 
 
-def identify_removed_snippets(original: Dict[str, List[str]], new: Dict[str, List[str]]) -> List[str]:
+def identify_removed_snippets(
+        old: Dict[str, List[str]],
+        new: Dict[str, List[str]]
+) -> List[str]:
     removed_files: List[str] = []
-    for file_path, hashes in original.items():
+    for file_path, hashes in old.items():
         if file_path not in new:
             removed_files.append(file_path)
     return removed_files
 
 
-def identify_changed_snippets(original: Dict[str, List[str]], new: Dict[str, List[str]]) -> List[str]:
+def identify_changed_files(
+        original: Dict[str, List[str]],
+        new: Dict[str, List[str]]) -> List[str]:
     changed_files: List[str] = []
     for file_path, hashes in new.items():
         if file_path not in original:
@@ -54,24 +59,11 @@ def identify_changed_snippets(original: Dict[str, List[str]], new: Dict[str, Lis
     return changed_files
 
 
-def create_dict_of_sources(ids: List[str]) -> Dict[str, List[str]]:
-    snippets: Dict[str, List[str]] = {}
-
-    for id in ids:
-        parts = id.rsplit(':', 1)
-        file_path = parts[0]
-        hash_value = parts[1]
-
-        if file_path in snippets:
-            snippets[file_path].append(hash_value)
-        else:
-            snippets[file_path] = [hash_value]
-
-    return snippets
-
-
-def load_source_code(repo_path: str, branch: Optional[str],
-                     source_identifier: str) -> (List[str], List[Document]):
+def load_source_code(
+        repo_path: str,
+        branch: Optional[str],
+        source_identifier: str
+) -> (List[str], List[Document]):
     loader = GitLoader(
         repo_path=repo_path,
         branch=branch,
@@ -102,7 +94,7 @@ def load_saved_ids():
     return lines
 
 
-def remove_docs(ids: List[str]):
+def delete_docs(ids: List[str]):
     from langchain.vectorstores import Chroma
     chroma = Chroma(persist_directory="./mynewdb")
     chroma.delete(ids)
@@ -119,103 +111,91 @@ def index_docs(ids: List[str], docs: List[Document]):
         embedding=embeddings,
         persist_directory="./mynewdb"
     )
-    # db.from_documents(docs)
 
 
-def filter_documents_by_id(documents: List[Document], ids: List[str]):
-    docs = zip(documents, ids)
+from typing import Dict, List, TypeVar
+
+T = TypeVar('T')
 
 
-def create_dict_of_files_to_docs_and_hashes(ids, docs) -> Dict[str, List[Tuple[Document, str]]]:
-    """Creates a dictionary of IDs and documents from two lists of IDs and documents.
+def create_dict_from_keys_and_values(keys: List[str], values: List[T]) -> Dict[str, List[T]]:
+    assert (len(keys) == len(values))
 
-  Args:
-    ids: A list of IDs.
-    docs: A list of documents.
-
-  Returns:
-    A dictionary of IDs and documents.
-  """
     id_doc_dict = {}
-    for i in range(len(ids)):
-        if ids[i].rsplit(':', 1)[0] in id_doc_dict:
-            current_value = id_doc_dict[ids[i].rsplit(':', 1)[0]]
-            current_value.append((docs[i], ids[i].rsplit(':', 1)[1]))
-            id_doc_dict[ids[i].rsplit(':', 1)[0]] = current_value
+
+    if len(keys) == 0:
+        return id_doc_dict
+
+    for i, file_path in enumerate(keys):
+        if file_path in id_doc_dict:
+            id_doc_dict[file_path].append(values[i])
         else:
-            id_doc_dict[ids[i].rsplit(':', 1)[0]] = [(docs[i], ids[i].rsplit(':', 1)[1])]
+            id_doc_dict[file_path] = [values[i]]
     return id_doc_dict
 
 
-def extract_info_from_id(id: str) -> (str, str, str):
-    r = id.split(':')
-    return r[0], r[1], r[2]
+def map_ids_to_paths(ids: List[str]) -> List[str]:
+    return [id.split(':')[2] for id in ids]
 
 
-def upsert_docs(ids: List[str], docs: List[Document], db_dir: str, doc_type: str = "source_code",
-                source_identifier: str = "github.com/zio/zio"):
-    new_files_to_docs_and_hashes_dicts: Dict[str, List[Tuple[Document, str]]] = create_dict_of_files_to_docs_and_hashes(
-        ids, docs)
-    new_files_to_hash_dicts: Dict[str, List[str]] = {key: [item[1] for item in value] for key, value in
-                                                     new_files_to_docs_and_hashes_dicts.items()}
+def map_ids_to_hashes(ids: List[str]) -> List[str]:
+    return [id.split(':')[3] for id in ids]
 
-    indexed_docs_ids: List[str] = get_original_ids(db_dir, doc_type, source_identifier)
-    indexed_file_to_hash_dict: Dict[str, List[str]] = create_dict_of_sources(indexed_docs_ids)
 
-    changed_files: List[str] = identify_changed_snippets(indexed_file_to_hash_dict, new_files_to_hash_dicts)
+def upsert_docs(ids: List[str], docs: List[Document], db_dir: str, doc_type: str, source_identifier: str):
+    assert (len(ids) == len(docs))
+
+    new_paths: List[str] = map_ids_to_paths(ids)
+    new_hashes: List[str] = map_ids_to_hashes(ids)
+    new_file_path_to_docs: Dict[str, list[Document]] = create_dict_from_keys_and_values(new_paths, docs)
+    new_file_path_to_hash: Dict[str, List[str]] = create_dict_from_keys_and_values(new_paths, new_hashes)
+
+    old_doc_ids: List[str] = get_original_ids(db_dir, doc_type, source_identifier)
+    old_paths: List[str] = map_ids_to_paths(old_doc_ids)
+    old_hashes: List[str] = map_ids_to_hashes(old_doc_ids)
+    old_file_path_to_hash: Dict[str, List[str]] = create_dict_from_keys_and_values(old_paths, old_hashes)
+
+    changed_files_paths: List[str] = identify_changed_files(
+        old_file_path_to_hash,
+        new_file_path_to_hash
+    )
 
     changed_docs_ids: List[str] = []
     changed_docs: List[Document] = []
-    for file in changed_files:
-        if file in new_files_to_docs_and_hashes_dicts:
-            d = new_files_to_docs_and_hashes_dicts[file]
-            for x in d:
-                changed_docs.append(x[0])
-                changed_docs_ids.append(file + ':' + x[1])
-        else:
-            print(file)
+    for file_path in changed_files_paths:
+        if file_path in new_file_path_to_docs:
+            for d in new_file_path_to_docs[file_path]:
+                changed_docs.append(d)
+            for h in new_file_path_to_hash[file_path]:
+                changed_docs_ids.append(doc_type + ':' + source_identifier + ':' + file_path + ':' + h)
 
     removed_files: List[str] = list(
-        set(identify_removed_snippets(indexed_file_to_hash_dict, new_files_to_hash_dicts) + changed_files))
+        set(
+            identify_removed_snippets(
+                old_file_path_to_hash, new_file_path_to_hash
+            ) + changed_files_paths
+        )
+    )
     removed_docs_ids: List[str] = []
-    for file in removed_files:
-        if file in indexed_file_to_hash_dict:
-            hashes = indexed_file_to_hash_dict[file]
-            for h in hashes:
-                removed_docs_ids.append(file + ':' + h)
+    for file_path in removed_files:
+        if file_path in old_file_path_to_hash:
+            for h in old_file_path_to_hash[file_path]:
+                removed_docs_ids.append(doc_type + ':' + source_identifier + ':' + file_path + ':' + h)
+
     if len(removed_docs_ids) != 0:
-        remove_docs(removed_docs_ids)
+        delete_docs(removed_docs_ids)
 
     assert (len(changed_docs) == len(changed_docs))
 
-    if len(changed_docs_ids)  != 0:
+    if len(changed_docs_ids) != 0:
         index_docs(changed_docs_ids, changed_docs)
 
 
 def start():
     ids, docs = load_source_code("/home/milad/sources/scala/zio", "series/2.x", "github.com/zio/zio")
     # index_docs(ids[0:9], docs[0:9])
-    upsert_docs(ids[0:9], docs[0:9], "./mynewdb")
+    upsert_docs(ids[0:9], docs[0:9], "./mynewdb", "source_code", "github.com/zio/zio")
 
 
 if __name__ == "__main__":
     start()
-    # ids = get_original_ids("./mynewdb", "source_code", "github.com/zio/zio")
-    # print(ids)
-    # dict = create_dict_of_sources(ids)
-    # print(dict)
-    # # start()
-
-
-def test():
-    # ids, _ = load_source_code("/home/milad/sources/scala/zio", "series/2.x", "github.com/zio/zio")
-
-    # with open("ids.txt", "w") as file:
-    #     for id in ids:
-    #         file.write(id + "\n")
-
-    original_ids = load_saved_ids()
-    original_dict = create_dict_of_sources(original_ids)
-    new_ids, _ = load_source_code("/home/milad/sources/scala/zio", "series/2.x", "github.com/zio/zio")
-    new_dict = create_dict_of_sources(new_ids)
-    changes = identify_changed_snippets(original_dict, new_dict)
--- bytebrain-server/tests/test_sample_text_splitter.py
+++ bytebrain-server/tests/test_sample_text_splitter.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import TestCase
-from dev.sample_text_splitter import identify_changed_snippets, identify_removed_snippets
+
+from dev.sample_text_splitter import identify_changed_files, identify_removed_snippets, map_ids_to_paths, \
+    create_dict_from_keys_and_values
 
 
 class TestIdentifyChanges(TestCase):
@@ -14,7 +16,7 @@ class TestIdentifyChanges(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, [])
 
     def test_identify_changed_snippets_added_files(self):
@@ -26,7 +28,7 @@ class TestIdentifyChanges(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, ['file2.txt'])
 
     def test_identify_changed_snippets_changed_hashes(self):
@@ -39,7 +41,7 @@ class TestIdentifyChanges(TestCase):
             'file1.txt': ['hash1', 'hash4'],  # hash2 changed to hash4
             'file2.txt': ['hash3']
         }
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, ['file1.txt'])
 
     def test_identify_changed_snippets_reduced_num_of_hashes(self):
@@ -52,7 +54,7 @@ class TestIdentifyChanges(TestCase):
             'file1.txt': ['hash1', 'hash2'],  # hash3 removed
             'file2.txt': ['hash3']
         }
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, ['file1.txt'])
 
     def test_identify_changed_snippets_different_file_counts(self):
@@ -64,21 +66,21 @@ class TestIdentifyChanges(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, ['file2.txt'])
 
     def test_identify_changed_snippets_empty_input(self):
         # Test when both original and new are empty dictionaries
         original = {}
         new = {}
-        result = identify_changed_snippets(original, new)
+        result = identify_changed_files(original, new)
         self.assertEqual(result, [])
 
 
 class TestIdentifyRemovedSnippets(TestCase):
     def test_identify_changed_snippets_removed_files(self):
         # Test when files are removed in the new dictionary
-        original = {
+        old = {
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3'],
             'file3.txt': ['hash4']
@@ -87,12 +89,12 @@ class TestIdentifyRemovedSnippets(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file3.txt': ['hash4']  # 'file2.txt' is removed
         }
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(['file2.txt'], result)
 
     def test_identify_removed_snippets_no_removed_files(self):
         # Test when there are no removed files
-        original = {
+        old = {
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
@@ -100,12 +102,12 @@ class TestIdentifyRemovedSnippets(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(result, [])
 
     def test_identify_removed_snippets_some_removed_files(self):
         # Test when some files are removed
-        original = {
+        old = {
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3'],
             'file3.txt': ['hash4']
@@ -114,29 +116,29 @@ class TestIdentifyRemovedSnippets(TestCase):
             'file1.txt': ['hash1', 'hash2'],
             'file3.txt': ['hash4']
         }
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(result, ['file2.txt'])
 
     def test_identify_removed_snippets_all_removed_files(self):
         # Test when all files are removed
-        original = {
+        old = {
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3']
         }
         new = {}
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(result, ['file1.txt', 'file2.txt'])
 
     def test_identify_removed_snippets_empty_input(self):
         # Test when both original and new are empty dictionaries
-        original = {}
+        old = {}
         new = {}
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(result, [])
 
     def test_identify_removed_snippets_hashes_changed(self):
         # Test when the original and new dictionaries have the same files, but only the hashes have changed
-        original = {
+        old = {
             'file1.txt': ['hash1', 'hash2'],
             'file2.txt': ['hash3', 'hash4'],
             'file3.txt': ['hash5']
@@ -146,8 +148,73 @@ class TestIdentifyRemovedSnippets(TestCase):
             'file2.txt': ['hash8', 'hash4'],  # Hashes changed for file2.txt
             'file3.txt': ['hash5']  # No change for file3.txt
         }
-        result = identify_removed_snippets(original, new)
+        result = identify_removed_snippets(old, new)
         self.assertEqual(result, [])
+
+
+class TestMapIdsToPaths(unittest.TestCase):
+
+    def test_empty_list(self):
+        # Test when the input list is empty
+        ids = []
+        result = map_ids_to_paths(ids)
+        self.assertEqual(result, [])
+
+    def test_single_id(self):
+        # Test when there is a single ID in the list
+        ids = [
+            "source_code:github.com/zio/zio:benchmarks/src/main/scala-2.12/zio/GenBenchmarks.scala:fbb107a47a17202f1b5f3a39f3345c09"
+        ]
+        result = map_ids_to_paths(ids)
+        self.assertEqual(result,
+                         ["benchmarks/src/main/scala-2.12/zio/GenBenchmarks.scala"])
+
+    def test_multiple_ids(self):
+        # Test when there are multiple IDs in the list
+        ids = [
+            "source_code:github.com/zio/zio:benchmarks/src/main/scala-2.12/zio/GenBenchmarks.scala",
+            "source_code:github.com/zio/zio:benchmarks/src/main/scala/zio/ArrayFillBenchmark.scala",
+            "source_code:github.com/zio/zio:benchmarks/src/main/scala/zio/AsyncConcurrentBenchmarks.scala"
+        ]
+        result = map_ids_to_paths(ids)
+        expected = [
+            "benchmarks/src/main/scala-2.12/zio/GenBenchmarks.scala",
+            "benchmarks/src/main/scala/zio/ArrayFillBenchmark.scala",
+            "benchmarks/src/main/scala/zio/AsyncConcurrentBenchmarks.scala"
+        ]
+        self.assertEqual(result, expected)
+
+
+class TestCreateDictFromKeysAndValues(unittest.TestCase):
+
+    def test_empty_lists(self):
+        paths = []
+        docs = []
+        result = create_dict_from_keys_and_values(paths, docs)
+        self.assertEqual(result, {})
+
+    def test_single_element(self):
+        paths = ['file1.txt']
+        docs = ['document1']
+        result = create_dict_from_keys_and_values(paths, docs)
+        expected = {'file1.txt': ['document1']}
+        self.assertEqual(result, expected)
+
+    def test_multiple_elements(self):
+        paths = ['file1.txt', 'file2.txt', 'file1.txt']
+        docs = ['document1', 'document2', 'document3']
+        result = create_dict_from_keys_and_values(paths, docs)
+        expected = {
+            'file1.txt': ['document1', 'document3'],
+            'file2.txt': ['document2']
+        }
+        self.assertEqual(result, expected)
+
+    def test_length_mismatch(self):
+        paths = ['file1.txt', 'file2.txt']
+        docs = ['document1']
+        with self.assertRaises(AssertionError):
+            create_dict_from_keys_and_values(paths, docs)
 
 
 if __name__ == '__main__':
