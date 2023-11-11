fe6e10cbf0e10a3093ab69d006d24ff58e0359a4
Milad Khajavi <khajavi@gmail.com>
2023-11-11 20:23:36 +0330

    update main.

--- bytebrain-server/main.py
+++ bytebrain-server/main.py
@@ -1,12 +1,12 @@
 import multiprocessing
 import core
-from core.webservice import main
-from core.discord_bot import main
+from core.bots.web.webservice import main
+from core.bots.discord.discord_bot import main
 
 
 def start():
-    p1 = multiprocessing.Process(target=core.webservice.main)
-    p2 = multiprocessing.Process(target=core.discord_bot.main)
+    p1 = multiprocessing.Process(target=core.bots.web.webservice.main)
+    p2 = multiprocessing.Process(target=core.bots.discord.discord_bot.main)
 
     p1.start()
     p2.start()
