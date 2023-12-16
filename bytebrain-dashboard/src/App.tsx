b53830ef8dc714ff46515a19f8efed3fae128502
Milad Khajavi <khajavi@gmail.com>
2023-12-16 15:17:40 +0330

    wip

---
+++ bytebrain-dashboard/src/App.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="text-foreground">
+      <p>
+        Hello
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
