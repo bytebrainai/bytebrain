43efc3cdb849b6e6356f4ec522dc85f9cbfa56f8
Milad Khajavi <khajavi@gmail.com>
2024-01-07 14:12:53 +0330

    new component: WebpageFrom.

--- bytebrain-dashboard/src/Resources.tsx
+++ bytebrain-dashboard/src/Resources.tsx
@@ -18,10 +18,10 @@ import {
 import { useToast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpWideNarrow, MoreHorizontal, PlusCircleIcon } from "lucide-react";
+import { MoreHorizontal, PlusCircleIcon } from "lucide-react";
 import moment from "moment";
 import { useState } from "react";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { DataTable } from "./app/data-table";
 
@@ -34,7 +34,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { useLocation, useParams, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import "./App.css";
 import { NavBar } from "./NavBar";
@@ -60,6 +60,7 @@ import {
 } from "@/components/ui/popover";
 
 import { cn } from "@/lib/utils";
+import WebpageForm from "./WebpageForm";
 import WebsiteForm from "./WebsiteForm";
 
 const frameworks = [
@@ -400,20 +401,6 @@ export function Resources(props: any) {
 
 
 
-  const webpageFormSchema = z.object({
-    name: z.string(),
-    url: z.string().url({ message: "Invalid URL" }),
-  });
-
-  const webpageForm = useForm<z.infer<typeof webpageFormSchema>>({
-    mode: "onSubmit",
-    resolver: zodResolver(webpageFormSchema),
-    defaultValues: {
-      name: "",
-      url: "",
-    },
-  });
-
 
   const githubFormSchema = z.object({
     name: z.string(),
@@ -453,56 +440,6 @@ export function Resources(props: any) {
 
 
 
-
-  async function createWebpageResource(access_token: string, name: string, url: string, project_id: string): Promise<Result<Resource, Error>> {
-    try {
-      const response = await fetch(`http://localhost:8081/resources/webpage`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: name,
-          url: url,
-          project_id: project_id,
-        })
-      })
-      if (response.ok) {
-        const responseData = await response.json();
-        return { value: responseData, error: null };
-      } else {
-        if (response.status === 401) {
-          return { value: null, error: new Unauthorized() }
-        }
-        return { value: null, error: Error(response.statusText) };
-      }
-    } catch (error) {
-      return { value: null, error: Error(JSON.stringify(error)) };
-    }
-  }
-
-  async function onSubmitWebpageForm(values: z.infer<typeof webpageFormSchema>) {
-    const access_token = localStorage.getItem("accessToken");
-    if (access_token) {
-      const result = await createWebpageResource(access_token, values.name, values.url, project.id);
-      if (result.value) {
-        toast({
-          description: "Successfully created resource",
-        });
-        updateProject();
-        setOpen(false);
-      } else {
-        toast({
-          description: "There was an error creating resource. Please try again!",
-        });
-      }
-    } else {
-      window.location.assign(
-        "http://localhost:5173/auth/login"
-      );
-    }
-  }
 
 
   async function createGitHubResource(
@@ -680,57 +617,7 @@ export function Resources(props: any) {
                       </TabsContent>
                       <TabsContent value="webpage" className="pt-3">
                         Crawl a url and ingest it.
-                        <Form {...webpageForm}>
-                          <form onSubmit={webpageForm.handleSubmit(onSubmitWebpageForm)}>
-                            <div className="grid w-full items-center gap-4 pt-5">
-                              <div className="flex flex-col space-y-1.5">
-                                <FormField
-                                  control={webpageForm.control}
-                                  name="name"
-                                  render={({ field, formState }) => (
-                                    <FormItem>
-                                      <FormLabel className="font-extrabold pb-2">Name</FormLabel>
-                                      <FormControl>
-                                        <Input
-                                          {...field}
-                                          placeholder="Resource Name"
-                                          autoCapitalize="none"
-                                          autoComplete="true"
-                                          autoCorrect="off"
-                                          disabled={formState.isSubmitting}
-                                        />
-                                      </FormControl>
-                                      <FormMessage />
-                                    </FormItem>
-                                  )}
-                                />
-                                <FormField
-                                  control={webpageForm.control}
-                                  name="url"
-                                  render={({ field, formState }) => (
-                                    <FormItem className="pt-4">
-                                      <FormLabel className="font-extrabold pb-2">URL</FormLabel>
-                                      <FormControl>
-                                        <Input
-                                          {...field}
-                                          placeholder="Website URL"
-                                          autoCapitalize="none"
-                                          autoComplete="true"
-                                          autoCorrect="off"
-                                          disabled={formState.isSubmitting}
-                                        />
-                                      </FormControl>
-                                      <FormMessage />
-                                    </FormItem>
-                                  )}
-                                />
-                                <div className="flex pt-3 pb-3">
-                                  <Button type="submit" className="justify-items-center">Create</Button>
-                                </div>
-                              </div>
-                            </div>
-                          </form>
-                        </Form>
+                        <WebpageForm project_id={project.id} updateProject={updateProject} setOpen={setOpen} />
                       </TabsContent>
                       <TabsContent value="github" className="pt-3">
                         Clone a public repository and ingest it.
@@ -954,5 +841,4 @@ export function Resources(props: any) {
 }
 
 export default Resources;
-
 
---
+++ bytebrain-dashboard/src/WebpageForm.tsx
@@ -0,0 +1,148 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Input } from "@/components/ui/input";
+
+import "./App.css";
+import { Resource, Result, Unauthorized } from "./Projects";
+
+("use client");
+
+
+function WebpageForm({ project_id, updateProject, setOpen }: { project_id: string, updateProject: () => void, setOpen: (open: boolean) => void }) {
+  const { toast } = useToast();
+
+  const webpageFormSchema = z.object({
+    name: z.string(),
+    url: z.string().url({ message: "Invalid URL" }),
+  });
+
+  const webpageForm = useForm<z.infer<typeof webpageFormSchema>>({
+    mode: "onSubmit",
+    resolver: zodResolver(webpageFormSchema),
+    defaultValues: {
+      name: "",
+      url: "",
+    },
+  });
+
+  async function createWebpageResource(access_token: string, name: string, url: string, project_id: string): Promise<Result<Resource, Error>> {
+    try {
+      const response = await fetch(`http://localhost:8081/resources/webpage`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          url: url,
+          project_id: project_id,
+        })
+      })
+      if (response.ok) {
+        const responseData = await response.json();
+        return { value: responseData, error: null };
+      } else {
+        if (response.status === 401) {
+          return { value: null, error: new Unauthorized() }
+        }
+        return { value: null, error: Error(response.statusText) };
+      }
+    } catch (error) {
+      return { value: null, error: Error(JSON.stringify(error)) };
+    }
+  }
+
+  async function onSubmitWebpageForm(values: z.infer<typeof webpageFormSchema>) {
+    const access_token = localStorage.getItem("accessToken");
+    if (access_token) {
+      const result = await createWebpageResource(access_token, values.name, values.url, project_id);
+      if (result.value) {
+        toast({
+          description: "Successfully created resource",
+        });
+        updateProject();
+        setOpen(false);
+      } else {
+        toast({
+          description: "There was an error creating resource. Please try again!",
+        });
+      }
+    } else {
+      window.location.assign(
+        "http://localhost:5173/auth/login"
+      );
+    }
+  }
+
+
+  return (
+    <Form {...webpageForm}>
+      <form onSubmit={webpageForm.handleSubmit(onSubmitWebpageForm)}>
+        <div className="grid w-full items-center gap-4 pt-5">
+          <div className="flex flex-col space-y-1.5">
+            <FormField
+              control={webpageForm.control}
+              name="name"
+              render={({ field, formState }) => (
+                <FormItem>
+                  <FormLabel className="font-extrabold pb-2">Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Resource Name"
+                      autoCapitalize="none"
+                      autoComplete="true"
+                      autoCorrect="off"
+                      disabled={formState.isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={webpageForm.control}
+              name="url"
+              render={({ field, formState }) => (
+                <FormItem className="pt-4">
+                  <FormLabel className="font-extrabold pb-2">URL</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Website URL"
+                      autoCapitalize="none"
+                      autoComplete="true"
+                      autoCorrect="off"
+                      disabled={formState.isSubmitting}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex pt-3 pb-3">
+              <Button type="submit" className="justify-items-center">Create</Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Form>
+  )
+
+
+}
+
+export default WebpageForm;
